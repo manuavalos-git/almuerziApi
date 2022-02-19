f2c203379ede79c29b4d5c410a6dcf3dd4bb9750
manuavalos-git <manuavalos-git@github.com>
2022-02-19 18:33:11 -0300

    elimine html

---
+++ api/index.js
@@ -0,0 +1,14 @@
+const express= require("express")
+const mongoose= require("mongoose")
+const bodyParser= require("body-parser")
+const cors= require("cors")
+
+const app=express()
+app.use(bodyParser.json)
+app.use(cors())
+
+mongoose.connect(precess.env.MONGO_URI,{userNewUrlParser: true,useUnifiedTopology: true})
+app.get("*",(require,response)=> {
+    response.send("hola mundo")
+})
+module.exports=app
