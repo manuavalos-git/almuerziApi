39cb1c678f9dc63dace246cf6cf846954c3c1b64
manuavalos-git <manuavalos-git@github.com>
2022-02-19 22:57:17 -0300

    2

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const express= require("express")
 const mongoose= require("mongoose")
 const bodyParser= require("body-parser")
 const cors= require("cors")
-module.exports=(re,res)=>{
-    res.send('hola mundo')
-}
 
 const app=express()
 app.use(bodyParser.json)
