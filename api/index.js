aea4c17f9ea1690ea1a75b2c53db94c06a60fc31
manuavalos-git <manuavalos-git@github.com>
2022-02-20 07:56:47 -0300

    git ig

--- api/index.js
+++ api/index.js
@@ -10,7 +10,6 @@ app.use(cors())
 app.get('*',(req, res) => {
         res.send("hola mundooo")
 })
-//const MONGO_URI='mongodb+srv://manu:BpxnU9F7qEtY8GDn@cluster0.dvpi2.mongodb.net/almuerzi-db?retryWrites=true&w=majority'
 mongoose.connect(process.env.MONGO_URI,{useNewUrlParser: true , useUnifiedTopology: true})
 
 module.exports=app
