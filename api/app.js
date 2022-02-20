68c64ff719997c8a95a4493d82721249291301c8
manuavalos-git <manuavalos-git@github.com>
2022-02-20 07:14:59 -0300

    subo a app

--- api/app.js
+++ api/app.js
@@ -1,1 +1,17 @@
 console.log("buena")
+const express= require("express")
+const mongoose= require("mongoose")
+const bodyParser= require("body-parser")
+const cors= require("cors")
+
+const app=express()
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('*',(req, res) => {
+        res.send("hola mundo")
+})
+const MONGO_URI='mongodb+srv://manu:BpxnU9F7qEtY8GDn@cluster0.dvpi2.mongodb.net/almuerzi-db?retryWrites=true&w=majority'
+mongoose.connect(MONGO_URI,{useNewUrlParser: true , useUnifiedTopology: true})
+
+module.exports=app
