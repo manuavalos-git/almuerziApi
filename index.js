6d3a3f1c0ac4fbef6f8b7cc121dc8e6f65b022df
manuavalos-git <manuavalos-git@github.com>
2022-02-19 23:22:21 -0300

    7

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors())
 app.get('/',(require,response)=> {
     response.send("hola mundo")
 })
-const MONGO_URI='mongodb+srv://manu:ZBNDmcQXCIpCG5Qy@cluster0.dvpi2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const MONGO_URI='mongodb+srv://manu:ZBNDmcQXCIpCG5Qy@cluster0.dvpi2.mongodb.net/almuerzi?retryWrites=true&w=majority'
 mongoose.connect(MONGO_URI,{useNewUrlParser: true , useUnifiedTopology: true})
 module.exports=app
 
