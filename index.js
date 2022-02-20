8a69dfdbf0b4c4430c9ffae0f018030d4e2dcec3
manuavalos-git <manuavalos-git@github.com>
2022-02-20 07:36:44 -0300

    descopio

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-// const express= require("express")
-// const mongoose= require("mongoose")
-// const bodyParser= require("body-parser")
-// const cors= require("cors")
+const express= require("express")
+const mongoose= require("mongoose")
+const bodyParser= require("body-parser")
+const cors= require("cors")
 
-// const app=express()
-// app.use(bodyParser.json())
-// app.use(cors())
+const app=express()
+app.use(bodyParser.json())
+app.use(cors())
 
-// app.get('*',(req, res) => {
-//         res.send("hola mundo")
-// })
+app.get('*',(req, res) => {
+        res.send("hola mundo")
+})
 
-// mongoose.connect(MONGO_URI,{useNewUrlParser: true , useUnifiedTopology: true})
+mongoose.connect(MONGO_URI,{useNewUrlParser: true , useUnifiedTopology: true})
 
-// module.exports=app
+module.exports=app
