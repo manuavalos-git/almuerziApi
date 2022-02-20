6c3bd1ec942f92f08642acf85cc941b64aac6a63
manuavalos-git <manuavalos-git@github.com>
2022-02-20 07:41:46 -0300

    process env

--- api/app.js
+++ api/app.js
@@ -1,4 +1,3 @@
-console.log("buena")
 const express= require("express")
 const mongoose= require("mongoose")
 const bodyParser= require("body-parser")
@@ -11,7 +10,7 @@ app.use(cors())
 app.get('*',(req, res) => {
         res.send("hola mundooo")
 })
-const MONGO_URI='mongodb+srv://manu:BpxnU9F7qEtY8GDn@cluster0.dvpi2.mongodb.net/almuerzi-db?retryWrites=true&w=majority'
-mongoose.connect(MONGO_URI,{useNewUrlParser: true , useUnifiedTopology: true})
+//const MONGO_URI='mongodb+srv://manu:BpxnU9F7qEtY8GDn@cluster0.dvpi2.mongodb.net/almuerzi-db?retryWrites=true&w=majority'
+mongoose.connect(procces.env.MONGO_URI,{useNewUrlParser: true , useUnifiedTopology: true})
 
 module.exports=app
