1d20641f902109dbce5732a29d71c5db315ce57c
manuavalos-git <manuavalos-git@github.com>
2022-02-20 07:38:38 -0300

    a ver

--- api/app.js
+++ api/app.js
@@ -1,3 +1,4 @@
+console.log("buena")
 const express= require("express")
 const mongoose= require("mongoose")
 const bodyParser= require("body-parser")
@@ -8,9 +9,9 @@ app.use(bodyParser.json())
 app.use(cors())
 
 app.get('*',(req, res) => {
-        res.send("hola soy app")
+        res.send("hola mundooo")
 })
-
+const MONGO_URI='mongodb+srv://manu:BpxnU9F7qEtY8GDn@cluster0.dvpi2.mongodb.net/almuerzi-db?retryWrites=true&w=majority'
 mongoose.connect(MONGO_URI,{useNewUrlParser: true , useUnifiedTopology: true})
 
 module.exports=app
