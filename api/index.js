17be550d3b5cd142ced8cd04de84ef3ae6a1b28e
manuavalos-git <manuavalos-git@github.com>
2022-02-22 03:23:02 -0300

    siuu

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const app=express()
 app.use(bodyParser.json())
 app.use(cors())
 app.get('*',(req, res) => {
-        res.send("hola mundooo")
+        res.send("SIIIIIIUUUUU")
 })
 mongoose.connect(process.env.MONGO_URI,{useNewUrlParser: true , useUnifiedTopology: true})
 
